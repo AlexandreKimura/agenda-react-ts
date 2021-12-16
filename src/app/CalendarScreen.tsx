e103e119c66de665dc824154064e8af05d9413a1
Alexandre Kimura <alexandremkimura@hotmail.com>
2021-12-16 00:19:53 -0300

    Initial commit

---
+++ src/app/CalendarScreen.tsx
@@ -0,0 +1,83 @@
+import { makeStyles } from '@mui/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import { Avatar, Box, Button, Checkbox, FormControlLabel, Icon, IconButton } from '@mui/material';
+
+const DAYS_OF_WEEK = ['DOM', 'SEG', 'TER', 'QUA', 'QUI', 'SEX', 'SÁB']
+
+const useStyles = makeStyles({
+  table: {
+    borderTop: '1px solid rgb(224, 224, 224)',
+    minHeight: "100%",
+    "& td ~ td, & th ~ th": {
+      borderLeft: "1px solid rgb(224, 224, 224)"
+    }
+  }
+})
+
+export function CalendarScreen() {
+  const classes = useStyles()
+
+  return (
+    <Box display='flex' height="100%" alignItems="stretch">
+      <Box borderRight='1px solid rgb(224, 224, 224)' width="16em" padding="8px 16px">
+        <h2>Agenda React</h2>
+        <Button variant='contained' color='primary'>
+          Novo evento
+        </Button>
+
+        <Box marginTop='64px'>
+          <h3>Agenda React</h3>
+          <FormControlLabel 
+            control={<Checkbox />}
+            label="Pessoal"
+          />
+          <FormControlLabel 
+            control={<Checkbox />}
+            label="Trabalho"
+          />
+        </Box>
+      </Box>
+      <TableContainer component={"div"}>
+        <Box display='flex' alignItems='center' padding='8px 16px'>
+          <Box>
+            <IconButton aria-label='Mês anterior'>
+              <Icon>chevron_left</Icon>
+            </IconButton>
+            <IconButton aria-label='Próximo mês'>
+              <Icon>chevron_right</Icon>
+            </IconButton>
+          </Box>
+          <Box flex='1' marginLeft='16px' component='h3'>Junho de 2021</Box>
+          <IconButton aria-label='Usuário'>
+            <Avatar>
+              <Icon>person</Icon>
+            </Avatar>
+          </IconButton>
+        </Box>
+        <Table className={classes.table} sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+          <TableHead>
+            <TableRow>
+              {DAYS_OF_WEEK.map(day => <TableCell align='center' key={day}>{day}</TableCell>)}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              {DAYS_OF_WEEK.map(day => <TableCell align='center' key={day}>X</TableCell>)}
+            </TableRow>
+            <TableRow>
+              {DAYS_OF_WEEK.map(day => <TableCell align='center' key={day}>X</TableCell>)}
+            </TableRow>
+            <TableRow>
+              {DAYS_OF_WEEK.map(day => <TableCell align='center' key={day}>X</TableCell>)}
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
+  )
+}
