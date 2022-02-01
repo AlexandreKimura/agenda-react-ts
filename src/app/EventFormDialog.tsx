730622b9e6365a4faf206118415a9bb04768c860
Alexandre Kimura <alexandremkimura@hotmail.com>
2022-01-31 22:54:06 -0300

    Treating event inside EventFormDialog component

--- src/app/EventFormDialog.tsx
+++ src/app/EventFormDialog.tsx
@@ -6,6 +6,7 @@ import DialogTitle from '@mui/material/DialogTitle';
 import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
 import { TextField } from '@mui/material';
 import { ICalendar } from './backend';
+import { useEffect, useState } from 'react';
 
 export interface IEditingEvent {
   id?: number
@@ -23,7 +24,13 @@ interface IEventFormDialog {
 
 export function EventFormDialog(props: IEventFormDialog) {
 
-  const { event, calendars, onClose } = props
+  const { calendars, onClose } = props
+
+  const [event, setEvent] = useState<IEditingEvent | null>(props.event)
+
+  useEffect(() => {
+    setEvent(props.event)
+  }, [props.event])
 
   return (
     <div>
@@ -45,6 +52,7 @@ export function EventFormDialog(props: IEventFormDialog) {
                 label="Data"
                 fullWidth
                 value={event.date}
+                onChange={(e) => setEvent({...event, date: e.target.value})}
               />
               <TextField 
                 autoFocus
@@ -52,19 +60,22 @@ export function EventFormDialog(props: IEventFormDialog) {
                 label="Descrição"
                 fullWidth
                 value={event.desc}
+                onChange={(e) => setEvent({...event, desc: e.target.value})}
               />
               <TextField 
                 type="time"
                 margin='normal'
                 label="Hora"
                 fullWidth
-                value={event.time}
+                value={event.time ?? ""}
+                onChange={(e) => setEvent({...event, time: e.target.value})}
               />
               <FormControl  margin='normal' fullWidth>
                 <InputLabel id='select-calendar'>Agenda</InputLabel>
                 <Select
                   labelId='select-calendar'
                   value={event.calendarId}
+                  onChange={(e) => setEvent({...event, calendarId: e.target.value as number})}
                 >
                   {calendars.map((calendar) => {
                     return <MenuItem key={calendar.id} value={calendar.id}>{calendar.name}</MenuItem>
