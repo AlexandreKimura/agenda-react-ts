5328ae9752699ff8e2da408c83331f07e56dc9eb
Alexandre Kimura <alexandremkimura@hotmail.com>
2022-02-01 21:07:25 -0300

    Treating session

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,19 +6,31 @@ import {
   Route
 } from "react-router-dom";
 import { getToday } from './dateFunctions';
+import { useEffect, useState } from 'react';
+import { getUserEndpoint } from './backend';
 
 function App() {
 
   const month = getToday().substring(0, 7);
+  const [hasSession, setHasSession]= useState(false)
 
-  return (
-    <Router>
-       <Routes>
-          <Route path="/calendar/:month" element={<CalendarScreen />} />
-          <Route path="*" element={<Navigate to={{ pathname: "/calendar/" + month}} />} />
-        </Routes>
-    </Router>
-  )
+  useEffect(() => {
+    getUserEndpoint().then(() => setHasSession(true), () => setHasSession(false))
+  }, [])
+
+  if(hasSession) {
+    return (
+      <Router>
+         <Routes>
+            <Route path="/calendar/:month" element={<CalendarScreen />} />
+            <Route path="*" element={<Navigate to={{ pathname: "/calendar/" + month}} />} />
+          </Routes>
+      </Router>
+    )
+  } else {
+    return <div>Login</div>
+  }
+  
 }
 
 export default App;
